60e383a6ab378b4999196ba554b228b43cbffc5c
afassfox <afassfox@gmail.com>
2025-05-11 03:09:09 +0300

    fixing usestate.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { HangmanWord } from "./components/HangmanWord";
 import { Keyboard } from "./components/Keyboard";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
+  const [wordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
 
